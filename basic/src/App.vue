2ba74803fd9f6978143b64ce7c31f58f4b7997ae
H A S A N <hasan25042019@gmail.com>
2024-02-15 22:16:05 +0600

    Initial commit

---
+++ basic/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{first_name}}</h1>
+  <button @click="getFullName">click</button>
+</template>
+
+<script>
+  export default{
+    data(){
+      return{
+        first_name: "Bill",
+        last_name: "Gates",
+        age: 65,
+        getFullName: ()=>{
+          alert("Bill Gates");
+        }
+      }
+    }
+  }
+</script>
