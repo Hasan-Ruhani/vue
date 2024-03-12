8cadd2987553ac9ccc3b91d7957405325addec00
H A S A N <hasan25042019@gmail.com>
2024-03-12 23:27:13 +0600

    fdg

---
+++ ostad/module_7/components_ui/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  
+  <button class="bg-blue-500" hover:bg-blue-700 text-withe font-bold py-2 px-4>
+    +
+  </button>
+  <span class=""> 1 </span>
+  <button class="bg-blue-500" hover:bg-blue-700 text-withe font-bold py-2 px-4>
+    -
+  </button>
+
+</template>
+
+<style scoped>
+
+</style>
