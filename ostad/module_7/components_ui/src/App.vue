f4a06dab78557d11570e08382595e3ab5e20dcf2
H A S A N <hasan25042019@gmail.com>
2024-03-13 23:11:49 +0600

    df

--- ostad/module_7/components_ui/src/App.vue
+++ ostad/module_7/components_ui/src/App.vue
@@ -1,16 +1,11 @@
 <script setup>
-
+  import ButtonCounter from './components/ButtonCounter.vue';
 </script>
 
 <template>
-  
-  <button class="bg-blue-500" hover:bg-blue-700 text-withe font-bold py-2 px-4>
-    +
-  </button>
-  <span class=""> 1 </span>
-  <button class="bg-blue-500" hover:bg-blue-700 text-withe font-bold py-2 px-4>
-    -
-  </button>
+
+  <ButtonCounter :start="16"/> <br/><br/>     <!-- if you pass a number value so variable should be binding like(:start) -->
+  <button-counter/> <br/><br/>           <!-- ButtonCounter & button-counter is same at vue -->
 
 </template>
 
