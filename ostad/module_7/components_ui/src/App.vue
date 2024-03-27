7ed7471e8b79fd0c18f35e20c0505456833100cf
H A S A N <hasan25042019@gmail.com>
2024-03-27 22:46:55 +0600

    initial commit

--- ostad/module_7/components_ui/src/App.vue
+++ ostad/module_7/components_ui/src/App.vue
@@ -7,7 +7,17 @@
       image:"https://static-01.daraz.com.bd/p/b60462ad3204dfd6bda71897787e40b1.jpg_750x400.jpg_.webp",
       location:"Korea",
       title:"This is a Korean Watch",
-      reating:5,
+      reating:2,
+      price:512,
+      add_to_cart:"www.facebook.com"
+
+    },
+
+    {
+      image:"https://static-01.daraz.com.bd/p/b60462ad3204dfd6bda71897787e40b1.jpg_750x400.jpg_.webp",
+      location:"Korea",
+      title:"This is a Korean Watch",
+      reating:4,
       price:512,
       add_to_cart:"www.facebook.com"
 
@@ -27,11 +37,14 @@
 
 <template>
 
-<Location v-for="item in data" :key="item" :title="item.title" :image="item.image" :location="item.location" :price="item.price" :add_to_cart="item.add_to_cart" :reating="item.reating"></Location>
+<!-- <Location v-for="item in data" :key="item" :title="item.title" :image="item.image" :location="item.location" :price="item.price" :add_to_cart="item.add_to_cart" :reating="item.reating"></Location> -->
+<div class="flex">
+<Location v-for="item in data" :key="item.title" v-bind="item"></Location>
+</div>
 
 <br/>
 
-  <ButtonCounter :start="16"/> <br/><br/>     <!-- if you pass a number value so variable should be binding like(:start) -->
+  <ButtonCounter :start="8"/> <br/><br/>     <!-- if you pass a number value so variable should be binding like(:start) -->
   <button-counter/> <br/><br/>           <!-- ButtonCounter & button-counter is same at vue -->
 
 </template>
--- ostad/module_7/components_ui/src/components/ButtonCounter.vue
+++ ostad/module_7/components_ui/src/components/ButtonCounter.vue
@@ -22,15 +22,15 @@
 </script>
 
 <template>
-  
-  <button @click="increase()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-    +
+
+  <button @click="decrease()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    -
   </button>
 
   <span class="px-5"> {{ count }} </span>
 
-  <button @click="decrease()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-    -
+  <button @click="increase()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    +
   </button>
 
 </template>
