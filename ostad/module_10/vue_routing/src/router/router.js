b27de22386c482f979892a6298a53454885740fe
H A S A N <hasan25042019@gmail.com>
2024-04-01 23:09:40 +0600

    initial commit

---
+++ ostad/module_10/vue_routing/src/router/router.js
@@ -0,0 +1,34 @@
+import {createRouter, createWebHistory} from 'vue-router';
+
+import Home from '../components/Home.vue'
+import About from '../components/About.vue'
+import Contact from '../components/Contact.vue'
+import Blog from '../components/Blog.vue'
+
+const routes = [
+    {
+        path: '/',
+        component: Home,
+        name: 'home'
+    },
+    {
+        path: '/about',
+        component: About,
+        name: 'about'
+    },
+    {
+        path: '/contact',
+        component: Contact
+    },
+    {
+        path: '/blog',
+        component: Blog
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: routes
+});
+
+export default router
