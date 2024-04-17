86787f6feff227364a701bd05bdfdf0fc44c13d6
H A S A N <hasan25042019@gmail.com>
2024-04-17 22:06:45 +0600

    initial commit

---
+++ ostad/module_11/authentication/src/router/router.js
@@ -0,0 +1,61 @@
+import {createRouter, createWebHistory} from 'vue-router';
+
+import Home from '../components/Home.vue'
+import About from '../components/About.vue'
+import Contact from '../components/Contact.vue'
+import Blogs from '../components/Blogs.vue'
+import Blog from '../components/Blog.vue'
+import RightSidebar from '../components/RightSidebar.vue'
+import Protected from '../components/Protected.vue'
+
+const routes = [
+    {
+        path: '/',
+        component: Home,
+        name: 'home'
+    },
+    {
+        path: '/about',
+        component: About,
+        name: 'about'
+    },
+    {
+        path: '/contact',
+        component: Contact,
+        // name: 'contact'
+    },
+    {
+        path: '/blogs',
+        components:{
+            default: Blogs,
+            right: RightSidebar
+        },
+        // name: 'blog'
+    },
+    {
+        path: '/blogs/tag/:tag',     // :tag use for pass dynamic url
+        components:{
+            default: Blogs,
+            right: RightSidebar
+        },
+        name: 'category'
+    },
+    {
+        path: '/blogs/:id',   
+        component: Blog,
+        name: 'blog'
+    },
+    {
+        path: '/protected',
+        component: Protected,
+        name: 'protected'
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: routes
+});
+
+export default router
+
