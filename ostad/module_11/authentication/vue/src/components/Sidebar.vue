dad02f5c1454d74d1e16b0cd1aa42f14a27ca055
H A S A N <hasan25042019@gmail.com>
2024-04-24 21:58:31 +0600

    initial commit

--- ostad/module_11/authentication/vue/src/components/Sidebar.vue
+++ ostad/module_11/authentication/vue/src/components/Sidebar.vue
@@ -7,35 +7,42 @@
 
 <section class="mt-[40px] text-white p-5">
     <ul class="flex flex-col space-y-5">
+        
     <li>
         <!-- <RouterLink to="/">Home</RouterLink> -->
         <RouterLink :to="{name: 'home'}">Home</RouterLink>
     </li>
+    
     <li>
         <!-- <RouterLink to="/about">About</RouterLink> -->
         <RouterLink :to="{name: 'about'}">About</RouterLink>
     </li>
+
     <li>
         <RouterLink to="/contact">Contact</RouterLink>
     </li>
     <li>
         <RouterLink to="/blogs">Blogs</RouterLink>
     </li>
+
     <li>
         <RouterLink :to="{name: 'protected'}">Protected</RouterLink>
     </li>
     <li v-if="auth.user?.role == 'admin'">
         <RouterLink :to="{name: 'admin'}">Admin</RouterLink>
     </li>
+    
     <li v-if="auth.user?.role == 'editor' || auth.user?.role == 'admin'">
         <RouterLink :to="{name: 'editor'}">Editor</RouterLink>
     </li>
     <li v-if="!auth.isAuthenticated">
         <RouterLink :to="{name: 'login'}">Login</RouterLink>
     </li>
+
     <li v-if="auth.isAuthenticated">
         <RouterLink @click="auth.logout()" :to="{name: 'logout'}">Logout</RouterLink>
     </li>
+
     </ul>
 </section>
     
