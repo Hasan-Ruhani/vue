7ac7bc9356006ffd88875527ca7a1b2ac7fcf546
H A S A N <hasan25042019@gmail.com>
2024-04-29 22:30:45 +0600

    initial commit

--- ostad/module_10/vue_routing/src/components/HelloWorld.vue
+++ ostad/module_10/vue_routing/src/components/HelloWorld.vue
@@ -18,7 +18,6 @@ const count = ref(0)
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
-
   <p>
     Check out
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
@@ -34,7 +33,9 @@ const count = ref(0)
 </template>
 
 <style scoped>
+
 .read-the-docs {
   color: #888;
 }
+
 </style>
