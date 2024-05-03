fe3c26b27ad510c514d1ab8b9f1b2111f4f2a2dc
H A S A N <hasan25042019@gmail.com>
2024-05-03 22:04:50 +0600

    jhgh

--- ostad/module_12/admin_panel/src/components/Chart/BarChartComponent.vue
+++ ostad/module_12/admin_panel/src/components/Chart/BarChartComponent.vue
@@ -6,12 +6,14 @@
 import {Chart, Title, Tooltip, Legend, BarElement,CategoryScale,LinearScale} from 'chart.js'
 import { Bar } from 'vue-chartjs'
 
+
 Chart.register(CategoryScale, LinearScale, BarElement,Title, Tooltip, Legend)
 
 const data = {
   labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
   datasets: [{label: 'My Bar Chart', backgroundColor: '#41b883', data: [40, 20, 30, 39, 10, 35, 23]}]
 }
+
 
 // JSON Array Kata-Kati--->
 const options = {
