3cb4b2aa6e7f6401fd5755d71076b2f1214712ce
H A S A N <hasan25042019@gmail.com>
2024-07-03 15:30:43 +0600

    yi

--- ostad/module_8/input_components/src/components/TextComponents.vue
+++ ostad/module_8/input_components/src/components/TextComponents.vue
@@ -5,6 +5,7 @@
     const emit = defineEmits(['update:modelValue']);
 </script>
 
+
 <template>
     <label>{{ label }}</label><br/>
     <input type="text" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" style="color: red;">
